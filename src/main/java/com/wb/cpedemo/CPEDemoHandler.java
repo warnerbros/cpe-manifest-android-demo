9538f5355ce1aee2f412b72ba03fc61a8968f940
Grant Cheng <grant.cheng@warnerbros.com>
2016-11-01 15:39:01 -0700

    - movie nextgen library activities back into Nextgen library manifest - let client handle clip share links

--- src/main/java/com/wb/cpedemo/CPEDemoHandler.java
+++ src/main/java/com/wb/cpedemo/CPEDemoHandler.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
 
 import com.wb.nextgenlibrary.interfaces.NextGenEventHandler;
@@ -50,5 +51,18 @@ public class CPEDemoHandler implements NextGenEventHandler {
 	public void userEventLog(String screen, String subScreen, String button, String action, String value){
 		//FlixsterLogger.d("NextGenLog", "Screen = " + screen + "\nsubScreen = " + subScreen + "\nbutton = " + button + "\naction = " + action + "\nvalue = " + value);
 		// log conviva, fabric, google analytics
+	}
+
+	public void handleShareLink(Activity activity, Fragment fragment, String shareUrl){
+		Intent share = new Intent(Intent.ACTION_SEND);
+		share.setType("text/plain");
+		share.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+		share.putExtra(Intent.EXTRA_SUBJECT, "Next Gen Share");
+		share.putExtra(Intent.EXTRA_TEXT, shareUrl);
+
+		if (fragment != null)
+			fragment.startActivity(Intent.createChooser(share, ""));
+		else if (activity != null)
+			activity.startActivity(Intent.createChooser(share, ""));
 	}
 }
